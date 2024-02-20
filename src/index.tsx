aefeacea6aac84bb589a86a923bc294727bcaebc
remjx <35121685+remjx@users.noreply.github.com>
2024-02-19 20:47:56 -0500

    update github actions versions

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import packageJson from '../package.json';
+
+console.log(`App version: ${packageJson.version}`);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
